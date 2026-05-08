de2d8c47c970cdecdaff4de1163e42149e58b00b
adamaja29 <adamnur7890@gmail.com>
2026-05-08 23:59:10 +0700

    backend: buat user controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,16 @@
 import User from "../models/UserModels.js";
 import argon2 from "argon2";
 
-export const getUsers = () => {};
+export const getUsers = (req, res) => {
+    try{
+        const response = await User.findAll({
+            attributes: ['uuid', 'name', 'email', 'role']
+        });
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+};
 
 export const getUserById = () => {};
 
